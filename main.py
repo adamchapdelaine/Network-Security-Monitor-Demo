3c082e1bdfd9f0635f98cee8363934cf5ab984e3
Adam Chapdelaine <chapdela@email.uscb.edu>
2026-04-03 12:42:40 -0400

    Refactor packet sniffing and ping request implementation

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 
 import sys
-from scapy.all import sr1,IP,ICMP
+from scapy.all import IP, ICMP, AsyncSniffer, send
 
-# IP envelope with ping request
-p=sr1(IP(dst=sys.argv[1])/ICMP())
-if p:
-    p.show()
+# sniffing packets from interface asynchronously, filtering only ICMP packets 
+captured_packets = AsyncSniffer(count=10, iface='VirtualBox Host-Only Ethernet Adapter', filter='icmp')
+captured_packets.start()
 
+# testing network traffic by sending a ping request to iface
+ping_request = send(IP(dst='192.168.56.101')/ICMP())
+
+# stopping the sniffer and showing the captured packets
+captured_packets.stop()
+captured_packets.results[1].show()
 
